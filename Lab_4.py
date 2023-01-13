7adc26065a4c3191f5500466a2f89bfe8613756f
ProCodeZero <111232459+ProCodeZero@users.noreply.github.com>
2023-01-13 22:37:43 +0300

    Add files via upload

---
+++ Lab_4.py
@@ -0,0 +1,10 @@
+# Lab_4
+
+def string_concatenation(str_1, str_2):
+    return str_1 + str_2
+
+
+input_str_1 = input("Введите первую строку: ")
+input_str_2 = input("Введите вторую строку: ")
+
+print(string_concatenation(input_str_1, input_str_2))
